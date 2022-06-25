653526c6961d12efabbbf54dcb5d1827dacd2c35
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-06-26 01:56:40 +0700

    Add Api & Fix Some HTML

---
+++ components/js/add_post.js
@@ -0,0 +1,24 @@
+// create getValue() to get value from id "input"
+
+function getValue() {
+    username = document.getElementById('name').innerText;
+    data = document.getElementById('detail').value;
+    // post request to endpoint https://hackathonfriend.herokuapp.com/posts with axios
+    // and return the response in the callback function
+    const API_URL = 'https://hackathonfriend.herokuapp.com' + '/posts';
+    fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: username,
+                detail: data
+            }),
+        })
+        .then(response => {
+            // redirect to /interesting
+            window.location.href = '/interesting';
+        })
+
+}
